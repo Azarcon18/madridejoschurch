7cdd7ded9ba85ff87497ddb2f3e1af85a00f72a9
Azarcon18 <azarconrob@gmail.com>
2024-11-18 22:51:07 +0800

    commmit

--- sql.php
+++ sql.php
@@ -13,17 +13,31 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Query to show all tables
-$sql = "SHOW TABLES FROM $dbname";
+// Query to fetch all data from the donations table
+$sql = "SELECT * FROM donations";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    echo "Tables in database '$dbname':<br>";
-    while ($row = $result->fetch_array()) {
-        echo $row[0] . "<br>";
+    // Display data in a table
+    echo "<table border='1'>";
+    echo "<tr>";
+    // Output table headers
+    while ($fieldInfo = $result->fetch_field()) {
+        echo "<th>" . $fieldInfo->name . "</th>";
     }
+    echo "</tr>";
+    
+    // Output table rows
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        foreach ($row as $value) {
+            echo "<td>" . htmlspecialchars($value) . "</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
 } else {
-    echo "No tables found in the database.";
+    echo "No data found in the donations table.";
 }
 
 // Close connection
