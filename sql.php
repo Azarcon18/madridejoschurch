c0b4a08432dd649190b73cdd9dbac9556e970e21
Azarcon18 <azarconrob@gmail.com>
2024-11-19 13:08:40 +0800

    commit

--- sql.php
+++ sql.php
@@ -13,18 +13,38 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// SQL query to get all table names
-$sql = "SHOW TABLES";
-$result = $conn->query($sql);
+// Function to display table contents
+function displayTable($conn, $tableName) {
+    echo "<h3>Table: $tableName</h3>";
+    $sql = "SELECT * FROM $tableName";
+    $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    echo "Tables in the database '$dbname':<br>";
-    // Fetch and display each table name
-    while ($row = $result->fetch_array()) {
-        echo $row[0] . "<br>";
+    if ($result && $result->num_rows > 0) {
+        echo "<table border='1'><tr>";
+        // Fetch column names
+        while ($field = $result->fetch_field()) {
+            echo "<th>" . htmlspecialchars($field->name) . "</th>";
+        }
+        echo "</tr>";
+
+        // Fetch rows
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            foreach ($row as $cell) {
+                echo "<td>" . htmlspecialchars($cell) . "</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "No data found in table '$tableName'.<br>";
     }
-} else {
-    echo "No tables found in the database '$dbname'.";
+}
+
+// List of tables to display
+$tables = ['appointment_schedules', 'baptism_schedules', 'wedding_schedules'];
+foreach ($tables as $table) {
+    displayTable($conn, $table);
 }
 
 // Close connection
