dad6ab5902bd70dd27b0d44a3e5f40c37a7dae5d
Azarcon18 <azarconrob@gmail.com>
2024-11-19 23:01:32 +0800

    commit

--- admin/registered_users/manage_appointment.php
+++ admin/registered_users/manage_appointment.php
@@ -1,7 +1,7 @@
 <?php
 require_once('../../config.php');
 if(isset($_GET['id']) && $_GET['id'] > 0){
-    $qry = $conn->query("SELECT r.*, t.sched_type from `appointment_request` r inner join `schedule_type` t on r.sched_type_id = t.id where r.id = '{$_GET['id']}' ");
+    $qry = $conn->query("SELECT r.*, t.sched_type from `registered_users` where r.id = '{$_GET['id']}' ");
     if($qry->num_rows > 0){
         foreach($qry->fetch_assoc() as $k => $v){
             $$k=$v;
