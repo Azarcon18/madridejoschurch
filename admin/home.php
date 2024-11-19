90ef3ce53cbc96dfaa745f44dde1456be7ffcf2d
Azarcon18 <azarconrob@gmail.com>
2024-11-19 12:14:44 +0800

    commit

--- admin/home.php
+++ admin/home.php
@@ -148,10 +148,10 @@
                     <span class="info-box-number text-right">
                     <?php
     // Query the total 'status = 1' appointments
-    $appointmentCount = $conn->query("SELECT count(id) as total FROM appointment_request WHERE status = 1")->fetch_assoc()['total'];
+    $appointmentCount = $conn->query("SELECT count(id) as total FROM appointment_schedules WHERE status = 1")->fetch_assoc()['total'];
 
     // Query the total 'status = 1' baptism schedules
-    $baptismCount = $conn->query("SELECT count(id) as total FROM baptism_schedules WHERE id IN (SELECT id FROM baptism_schedules WHERE 1)")->fetch_assoc()['total'];
+    $baptismCount = $conn->query("SELECT count(id) as total FROM baptism_schedule WHERE id IN (SELECT id FROM baptism_schedule WHERE 1)")->fetch_assoc()['total'];
 
     // Combine the totals
     $combinedTotal = $appointmentCount + $baptismCount;
@@ -159,7 +159,7 @@
     // Output the combined total
     echo  number_format($combinedTotal);
 ?>
-``
+
 
 
                     </span>
