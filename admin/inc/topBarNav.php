0e981492780927fa95a8554953af7e410fc1c4d8
Azarcon18 <azarconrob@gmail.com>
2024-11-21 22:57:36 +0800

    commit

--- admin/inc/topBarNav.php
+++ admin/inc/topBarNav.php
@@ -1,57 +1,5 @@
-<?php
-// Database connection
-require_once 'initialize.php';
-
-// Fetch pending requests from burial, baptism, and wedding tables
-function fetch_pending_requests($conn) {
-    $pending_requests = [
-        'burial' => [],
-        'baptism' => [],
-        'wedding' => []
-    ];
-
-    // Fetch pending burial requests
-    $burial_query = "SELECT id, name FROM burial WHERE status = 'pending'";
-    $result = $conn->query($burial_query);
-    while ($row = $result->fetch_assoc()) {
-        $pending_requests['burial'][] = [
-            'id' => $row['id'],
-            'name' => $row['name'],
-            'url' => "admin/?page=burial&id=" . $row['id']
-        ];
-    }
-
-    // Fetch pending baptism requests
-    $baptism_query = "SELECT id, name FROM baptism WHERE status = 'pending'";
-    $result = $conn->query($baptism_query);
-    while ($row = $result->fetch_assoc()) {
-        $pending_requests['baptism'][] = [
-            'id' => $row['id'],
-            'name' => $row['name'],
-            'url' => "admin/?page=baptism&id=" . $row['id']
-        ];
-    }
-
-    // Fetch pending wedding requests
-    $wedding_query = "SELECT id, name FROM wedding WHERE status = 'pending'";
-    $result = $conn->query($wedding_query);
-    while ($row = $result->fetch_assoc()) {
-        $pending_requests['wedding'][] = [
-            'id' => $row['id'],
-            'name' => $row['name'],
-            'url' => "admin/?page=wedding&id=" . $row['id']
-        ];
-    }
-
-    return $pending_requests;
-}
-
-// Call the function to fetch pending requests
-$pending_requests = fetch_pending_requests($conn);
-?>
-
 <style>
-  .user-img {
+  .user-img{
         position: absolute;
         height: 27px;
         width: 27px;
@@ -59,75 +7,67 @@ $pending_requests = fetch_pending_requests($conn);
         left: -7%;
         top: -12%;
   }
-  .btn-rounded {
+  .btn-rounded{
         border-radius: 50px;
   }
 </style>
 <!-- Navbar -->
-<nav class="main-header navbar navbar-expand navbar-blue border border-light border-top-0 border-left-0 border-right-0 navbar-light text-sm">
-  <!-- Left navbar links -->
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-    </li>
-    <li class="nav-item d-none d-sm-inline-block">
-      <a href="<?php echo base_url ?>" class="nav-link">
-        <?php echo (!isMobileDevice()) ? $_settings->info('name') : $_settings->info('short_name'); ?> - Admin
-      </a>
-    </li>
-  </ul>
-  <!-- Right navbar links -->
-  <ul class="navbar-nav ml-auto">
-    <!-- Notification Bell -->
-    <li class="nav-item dropdown">
-      <a class="nav-link" href="#" id="notificationBell" data-toggle="dropdown" aria-expanded="false">
-        <i class="fas fa-bell"></i>
-        <span id="notificationCount" class="badge badge-danger navbar-badge">
-          <?php 
-          $total_pending = count($pending_requests['burial']) + count($pending_requests['baptism']) + count($pending_requests['wedding']);
-          echo $total_pending > 0 ? $total_pending : ''; 
-          ?>
-        </span>
-      </a>
-      <div class="dropdown-menu dropdown-menu-right" id="notificationMenu">
-        <span class="dropdown-item dropdown-header">Pending Requests</span>
-        <div id="notificationItems" class="dropdown-item text-wrap">
-          <?php 
-          foreach ($pending_requests as $type => $requests) {
-            foreach ($requests as $request) {
-              echo '<div class="dropdown-item">
-                      <b>' . ucfirst($type) . '</b>: ' . $request['name'] . '
-                      <a href="' . $request['url'] . '" class="ml-2 text-primary">View</a>
-                    </div>';
-            }
-          }
-          ?>
-        </div>
-        <div class="dropdown-divider"></div>
-        <a href="#" class="dropdown-item text-center text-primary" id="viewAllRequests">View All</a>
-      </div>
-    </li>
-    <!-- User Profile -->
-    <li class="nav-item">
-      <div class="btn-group nav-link">
-        <button type="button" class="btn btn-rounded badge badge-light dropdown-toggle dropdown-icon" data-toggle="dropdown">
-          <span>
-            <img src="<?php echo validate_image($_settings->userdata('avatar')) ?>" class="img-circle elevation-2 user-img" alt="User Image">
-          </span>
-          <span class="ml-3"><?php echo ucwords($_settings->userdata('firstname').' '.$_settings->userdata('lastname')) ?></span>
-          <span class="sr-only">Toggle Dropdown</span>
-        </button>
-        <div class="dropdown-menu" role="menu">
-          <a class="dropdown-item" href="<?php echo base_url.'admin/?page=user' ?>">
-            <span class="fa fa-user"></span> My Account
-          </a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="<?php echo base_url.'/classes/Login.php?f=logout' ?>">
-            <span class="fas fa-sign-out-alt"></span> Logout
-          </a>
-        </div>
-      </div>
-    </li>
-  </ul>
-</nav>
-<!-- /.navbar -->
+      <nav class="main-header navbar navbar-expand navbar-blue border border-light border-top-0  border-left-0 border-right-0 navbar-light text-sm">
+        <!-- Left navbar links -->
+        <ul class="navbar-nav">
+          <li class="nav-item">
+          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+          </li>
+          <li class="nav-item d-none d-sm-inline-block">
+            <a href="<?php echo base_url ?>" class="nav-link"><?php echo (!isMobileDevice()) ? $_settings->info('name'):$_settings->info('short_name'); ?> - Admin</a>
+          </li>
+        </ul>
+        <!-- Right navbar links -->
+        <ul class="navbar-nav ml-auto">
+          <!-- Navbar Search -->
+          <!-- <li class="nav-item">
+            <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+            <i class="fas fa-search"></i>
+            </a>
+            <div class="navbar-search-block">
+              <form class="form-inline">
+                <div class="input-group input-group-sm">
+                  <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+                  <div class="input-group-append">
+                    <button class="btn btn-navbar" type="submit">
+                    <i class="fas fa-search"></i>
+                    </button>
+                    <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                    <i class="fas fa-times"></i>
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </li> -->
+          <!-- Messages Dropdown Menu -->
+          <li class="nav-item">
+            <div class="btn-group nav-link">
+                  <button type="button" class="btn btn-rounded badge badge-light dropdown-toggle dropdown-icon" data-toggle="dropdown">
+                    <span><img src="<?php echo validate_image($_settings->userdata('avatar')) ?>" class="img-circle elevation-2 user-img" alt="User Image"></span>
+                    <span class="ml-3"><?php echo ucwords($_settings->userdata('firstname').' '.$_settings->userdata('lastname')) ?></span>
+                    <span class="sr-only">Toggle Dropdown</span>
+                  </button>
+                  <div class="dropdown-menu" role="menu">
+                    <a class="dropdown-item" href="<?php echo base_url.'admin/?page=user' ?>"><span class="fa fa-user"></span> My Account</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="<?php echo base_url.'/classes/Login.php?f=logout' ?>"><span class="fas fa-sign-out-alt"></span> Logout</a>
+                  </div>
+              </div>
+          </li>
+          <li class="nav-item">
+            
+          </li>
+         <!--  <li class="nav-item">
+            <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+            <i class="fas fa-th-large"></i>
+            </a>
+          </li> -->
+        </ul>
+      </nav>
+      <!-- /.navbar -->
