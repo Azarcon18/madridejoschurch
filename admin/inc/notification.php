0557dfabcb0f1dd51faa0a089acf09b3883e577b
Azarcon18 <azarconrob@gmail.com>
2024-11-21 13:56:49 +0800

    commit

--- admin/inc/notification.php
+++ admin/inc/notification.php
@@ -3,5 +3,6 @@ header('Content-Type: application/json');
 $burial_count = $conn->query("SELECT COUNT(*) FROM burial_requests WHERE status = '0'")->fetchColumn();
 $baptism_count = $conn->query("SELECT COUNT(*) FROM baptism_requests WHERE status = '0'")->fetchColumn();
 $wedding_count = $conn->query("SELECT COUNT(*) FROM wedding_requests WHERE status = '0'")->fetchColumn();
-echo json_encode(['0' => $burial_count, '0' => $baptism_count, '0' => $wedding_count]);
+echo json_encode(['burial' => $burial_count, 'baptism' => $baptism_count, 'wedding' => $wedding_count]);
 ?>
+0
