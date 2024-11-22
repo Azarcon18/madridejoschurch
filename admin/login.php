d7aa43f86fc8ab9179a75745fd5c41592562c773
Azarcon18 <azarconrob@gmail.com>
2024-11-22 22:44:31 +0800

    commit

--- admin/login.php
+++ admin/login.php
@@ -1,24 +1,4 @@
-<?php
-session_start();
-require_once('../config.php');
-
-// Handle login logic
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $username = mysqli_real_escape_string($conn, $_POST['username']);
-    $password = md5($_POST['password']); // Make sure this matches your DB storage method
-
-    $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
-    $result = mysqli_query($conn, $query);
-
-    if (mysqli_num_rows($result) > 0) {
-        $_SESSION['user'] = $username;
-        header('Location: dashboard.php');
-        exit();
-    } else {
-        $error_message = "Invalid username or password";
-    }
-}
-?>
+<?php require_once('../config.php'); ?>
 <!DOCTYPE html>
 <html lang="en" style="height: auto;">
 <?php require_once('inc/header.php'); ?>
@@ -116,7 +96,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   }
 </style>
 <body class="hold-transition login-page">
+  <script>
+    start_loader();
+  </script>
+
   <div class="login-box">
+    <!-- /.login-logo -->
     <div class="card card-outline card-primary">
       <div class="card-header text-center">
         <a href="./" class="h1"><b>Login</b></a>
@@ -124,15 +109,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
       <div class="card-body">
         <p class="login-box-msg">Sign in to start your session</p>
 
-        <?php 
-        if (isset($error_message)) {
-            echo "<p style='color: red; text-align: center;'>$error_message</p>";
-        }
-        ?>
-
-        <form id="login-frm" method="post">
+        <form id="login-frm" action="login_action.php" method="post"> <!-- Action added for form submission -->
           <div class="input-group mb-3">
-            <input type="text" class="form-control" name="username" placeholder="Username" required autofocus>
+            <input type="text" class="form-control" name="username" placeholder="Username" required autofocus> <!-- Added autofocus for user experience -->
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-user"></span>
@@ -140,7 +119,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             </div>
           </div>
           <div class="input-group mb-3">
-            <input type="password" class="form-control" name="password" placeholder="Password" required>
+            <input type="password" class="form-control" name="password" placeholder="Password" required> <!-- Required attribute for better validation -->
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-lock"></span>
@@ -151,14 +130,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <div class="col-8">
               <a href="<?php echo base_url; ?>">Go to Website</a>
             </div>
+            <!-- /.col -->
             <div class="col-4">
               <button type="submit" class="btn btn-primary btn-block">Sign In</button>
             </div>
+            <!-- /.col -->
           </div>
         </form>
+        <!-- /.social-auth-links -->
       </div>
+      <!-- /.card-body -->
     </div>
+    <!-- /.card -->
   </div>
+  <!-- /.login-box -->
 
   <!-- jQuery -->
   <script src="plugins/jquery/jquery.min.js"></script>
@@ -166,5 +151,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
   <!-- AdminLTE App -->
   <script src="dist/js/adminlte.min.js"></script>
+
+  <script>
+    $(document).ready(function(){
+      end_loader();
+    });
+  </script>
 </body>
 </html>
