4ff8832032858f6281bd2bcd82a461b24de8ac5d
Azarcon18 <azarconrob@gmail.com>
2024-12-09 01:16:13 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -23,6 +23,6 @@ function deleteFolderContents($folderPath) {
 }
 
 // Example usage
-$folderToDelete = __DIR__ . 'https://icpmadridejos.com/uploads/blog_uploads/YSzqldklKk/1629176691_warehouse-portrait.jpg	';
+$folderToDelete = __DIR__ . 'uploads/blog_uploads/YSzqldklKk/1629176691_warehouse-portrait.jpg';
 deleteFolderContents($folderToDelete);
 ?>
