e8ced2527e2a61e04f7b333fa4a18c4317554a76
Azarcon18 <azarconrob@gmail.com>
2024-12-09 00:47:39 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,88 +1,54 @@
-<?php require_once('../config.php'); ?>
- <!DOCTYPE html>
-<html lang="en" class="" style="height: auto;">
-<?php require_once('inc/header.php') ?>
-  <body class="sidebar-mini layout-fixed control-sidebar-slide-open layout-navbar-fixed light-mode sidebar-mini-md sidebar-mini-xs" data-new-gr-c-s-check-loaded="14.991.0" data-gr-ext-installed="" style="height: auto;">
-    <div class="wrapper">
-     <?php require_once('inc/topBarNav.php') ?>
-     <?php require_once('inc/navigation.php') ?>
-              
-     <?php $page = isset($_GET['page']) ? $_GET['page'] : 'home';  ?>
-      <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper bg-light pt-3" style="min-height: 567.854px;">
-     
-        <!-- Main content -->
-        <section class="content  text-dark">
-          <div class="container-fluid">
-            <?php 
-              if(!file_exists($page.".php") && !is_dir($page)){
-                  include '404.html';
-              }else{
-                if(is_dir($page))
-                  include $page.'/index.php';
-                else
-                  include $page.'.php';
+<?php
+// Database connection parameters
+$servername = "localhost";
+$username = "u510162695_church_db";
+$password = "1Church_db";
+$dbname = "u510162695_church_db";
 
-              }
-            ?>
-          </div>
-        </section>
-        <!-- /.content -->
-  <div class="modal fade" id="confirm_modal" role='dialog'>
-    <div class="modal-dialog modal-md modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title">Confirmation</h5>
-      </div>
-      <div class="modal-body">
-        <div id="delete_content"></div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id='confirm' onclick="">Continue</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-      </div>
-    </div>
-  </div>
-  <div class="modal fade" id="uni_modal" role='dialog'>
-    <div class="modal-dialog modal-md modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title"></h5>
-      </div>
-      <div class="modal-body">
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id='submit' onclick="$('#uni_modal form').submit()">Save</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-      </div>
-      </div>
-    </div>
-  </div>
-  <div class="modal fade" id="uni_modal_right" role='dialog'>
-    <div class="modal-dialog modal-full-height  modal-md" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title"></h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span class="fa fa-arrow-right"></span>
-        </button>
-      </div>
-      <div class="modal-body">
-      </div>
-      </div>
-    </div>
-  </div>
-  <div class="modal fade" id="viewer_modal" role='dialog'>
-    <div class="modal-dialog modal-md" role="document">
-      <div class="modal-content">
-              <button type="button" class="btn-close" data-dismiss="modal"><span class="fa fa-times"></span></button>
-              <img src="" alt="">
-      </div>
-    </div>
-  </div>
-      </div>
-      <!-- /.content-wrapper -->
-      <?php require_once('inc/footer.php') ?>
-  </body>
-</html>
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get list of all tables
+$tables_query = "SHOW TABLES";
+$tables_result = $conn->query($tables_query);
+
+if ($tables_result->num_rows > 0) {
+    while ($table = $tables_result->fetch_array()) {
+        echo "Table: " . $table[0] . "\n";
+        
+        // Retrieve and display data from each table
+        $data_query = "SELECT * FROM `" . $table[0] . "`";
+        $data_result = $conn->query($data_query);
+        
+        if ($data_result->num_rows > 0) {
+            // Print column headers
+            $fields = $data_result->fetch_fields();
+            foreach ($fields as $field) {
+                echo $field->name . "\t";
+            }
+            echo "\n";
+            
+            // Print data rows
+            while ($row = $data_result->fetch_assoc()) {
+                foreach ($row as $value) {
+                    echo $value . "\t";
+                }
+                echo "\n";
+            }
+        } else {
+            echo "No data in this table.\n";
+        }
+        echo "\n";
+    }
+} else {
+    echo "No tables found in the database.";
+}
+
+// Close connection
+$conn->close();
+?>
